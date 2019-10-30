6ce7a749d35468084405d691191376d017777932
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-30 18:45:59 -0300

    Adds routing to login and timeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Timeline from './pages/Timeline';
 
 function App() {
-  return (<h1>Hello World!</h1>);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login}></Route>
+        <Route path="/timeline" component={Timeline}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
