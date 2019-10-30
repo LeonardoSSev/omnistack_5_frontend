c54fe1422162251a558811b9fadfeb56d83c0362
Leonardo Santos Severino <leonardossev@gmail.com>
2019-10-30 18:53:59 -0300

    Saves username in localstorage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,36 @@ import twitterLogo from '../twitter.svg';
 import './Login.css';
 
 export default class Login extends Component {
+  state = {
+    username: ''
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    
+    const { username } = this.state;
+
+    if (!username.length) {
+      return;
+    }
+
+    localStorage.setItem('@GoTwitter:username', username);
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ username: event.target.value });
+  }
+
   render() {
     return (
       <div className="login-wrapper">
         <img src={twitterLogo} alt="GoTwitter"/>
-        <form>
-          <input placeholder="Nome do usuário"/>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Nome do usuário"
+            value={this.state.username}
+            onChange={this.handleInputChange}  
+          />
           <button type="submit">Entrar</button>
         </form>
       </div>
